3292217474798eeb90e270a2669177b5ade50d1d
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-24 02:28:31 -0700

    Fixed char array and its use as well as implemented prices array

--- Bill.c
+++ Bill.c
@@ -9,19 +9,11 @@ int main(int argc, char *argv[])
     double tax;
     double tip;
 
-    char meals[4];
-    strcopy(meals[0], "Salad");
-    strcopy(meals[1], "Soup");
-    strcopy(meals[2], "Sandwhich");
-    strcopy(meals[3], "Pizza");
-    
-    double prices[] = {9.95, 4.55, 13.25, 22.35};
-    
     /*
     * If there are not 3 arguments in command line (exluding string call)
     * default values will be used
     */
-    if(argc != 4){
+    if(argc != 3){
         tax = defaultPercentage;
         tip = defaultPercentage;
     }else
@@ -30,9 +22,30 @@ int main(int argc, char *argv[])
         tax = strtod(argv[1], NULL);
         tip = strtod(argv[2], NULL);
     }
+
+    /* 
+    * initialized char array of length 4x10
+    * to hold values of food items
+    */
+    char meals[4][10];
+    strcpy(meals[0], "Salad");
+    strcpy(meals[1], "Soup");
+    strcpy(meals[2], "Sandwhich");
+    strcpy(meals[3], "Pizza");
+
+    // initizialized double array to hold prices
+    double prices[] = {9.95, 4.55, 13.25, 22.35};
+    
+    char meal[1][10];
+    double price = prices[0];
+
+    strcpy(meal[0], meals[0]);
+
+    // Gets total prices of of meal
+    double taxPrice = price + (price*tax);
     double totalPrice = taxPrice + (taxPrice*tip);
 
-    printf("The %s is priced at: %.2f\n", meal, price);
+    printf("The %s is priced at: %.2f\n", meal[0], price);
     printf("Tax is: %.2f%%\n", tax);
     printf("Tip is: %.2f%%\n", tip);
     printf("Total price is: %.2f\n", totalPrice);
