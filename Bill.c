4acaa8dbe73ee23209d1a37c99b2f55293c0f81d
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-24 01:33:52 -0700

    Removed unnecessary code, variables no longer pointers

--- Bill.c
+++ Bill.c
@@ -4,27 +4,21 @@
 int main(int argc, double *argv[])
 {
     double defaultPercentage = 0.10;
-    double *tax;
-    double *tip;
-    double *meal;
+    double tax;
+    double tip;
+    double meal;
 
     if(argc < 4){
-        *tax = defaultPercentage;
-        *tip = defaultPercentage;
-        *meal = 20.99;
+        tax = defaultPercentage;
+        tip = defaultPercentage;
+        meal = 20.99;
     }else
     {
-        
-
+        tax = *argv[1];
+        tip = *argv[2];
+        meal = *argv[3];
     }
     
-
-
-    int counter = 1;
-    while(counter < argc){
-        printf("%s", argv[counter]);
-        counter++;
-    }
 
     return 0;
 }
