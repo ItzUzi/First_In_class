d96387f15a930dd078ac8402e730350f810179c1
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-24 11:19:33 -0700

    Added while loop and a way to take user input without parsing

--- NumberGuess.c
+++ NumberGuess.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /*
 * Interactive number guessing game in which user
@@ -9,9 +11,25 @@
 int main(int argc, char const *argv[])
 {
     int option;
-    printf("Welcome to my number guesser");
+    printf("Welcome to my number guesser\n");
     printf("Press 1 to play a game\n");
     printf("Press 2 to change the max number\n");
     printf("Press 3 to quit\n");
-    return 0;
+
+    while (option != 3)
+    {
+        char input;
+        printf("Please enter either 1-3: ");
+        scanf("%s", &input);
+        option = atoi(&input);
+        if (option != 1 && option != 2 && option != 3)
+        {
+            printf("\n\n\nPlease input a number between 1 - 3\n\n\n");
+            continue;
+        }
+
+    
+    }
+    
+    return 0; 
 }
