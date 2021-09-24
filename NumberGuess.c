7ea76aa2dee55eff192863aef508ccb23f3c1bfa
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-24 10:35:06 -0700

    Made new number guesser game, added outline within program

---
+++ NumberGuess.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+* Interactive number guessing game in which user
+* will guess number from 1-10 unless changed by user using 
+* the change method
+*/
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
